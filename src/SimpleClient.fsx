6636688dc3e81ff57714233edf0929be6ae8ec6d
John Wostenberg <jwosty@users.noreply.github.com>
2021-10-19 17:58:28 -0600

    Improve examples

--- src/SimpleClient.fsx
+++ src/SimpleClient.fsx
@@ -5,13 +5,12 @@ open System.Threading
 
 let client = new OscUdpClient("127.0.0.1", 12345)
 
-client.SendMessage { addressPattern = "/button1"; arguments = [OscString "Hello, world!"]  }
-
+client.SendMessage { addressPattern = "/window/openurl"; arguments = [OscString "https://github.com/jwosty/Osc.fs"] }
 
 let rand = Random()
 
 for _ in 0..9 do
-    let msg = { addressPattern = "/filter"; arguments = [OscFloat32 (float32 (rand.NextDouble()))] }
+    let msg = { addressPattern = "/oscillator/{1,2}/frequency"; arguments = [OscFloat32 (float32 (rand.NextDouble()))] }
     printfn "Sending message"
     client.SendMessage msg |> ignore
     Thread.Sleep 1_000
--- src/SimpleServer.fsx
+++ src/SimpleServer.fsx
@@ -1,6 +1,7 @@
 ﻿#load "Osc.fs"
 open Osc.fs
-open System.Runtime
+open System
+open System.Diagnostics
 
 let (|OscNumber|_|) arg =
     match arg with
@@ -10,21 +11,39 @@ let (|OscNumber|_|) arg =
 
 let argsStr args = args |> Seq.map (fun arg -> string arg) |> String.concat "," |> sprintf "[%s]"
 
+let oscillator oscillatorId =
+    Path (oscillatorId, [
+        Method ("frequency", (fun msg -> async {
+            match msg.arguments with
+            | [OscNumber value] -> printfn "oscillator %s frequency: %f" oscillatorId value
+            | _ -> eprintfn "%s : wrong data type" msg.addressPattern
+        }))
+        Method ("amplitude", (fun msg -> async {
+            match msg.arguments with
+            | [OscNumber value] -> printfn "oscillator %s amplitude: %f" oscillatorId value
+            | _ -> eprintfn "%s : wrong data type" msg.addressPattern
+        }))
+    ])
+
 let server =
     new OscUdpServer("127.0.0.1", 12345, [
-        Method ("things/1/value", (fun msg -> async {
-            match msg.arguments with
-            | [OscNumber value] -> printfn "New value for thing 1: %f" value
-            | _ -> eprintfn "%s : wrong data type" msg.addressPattern
-        }))
-        Method ("things/2/value", (fun msg -> async {
-            match msg.arguments with
-            | [OscNumber value] -> printfn "New value for thing 2: %f" value
-            | _ -> eprintfn "%s : wrong data type" msg.addressPattern
-        }))
-        Method ("button1", (fun msg -> async {
-            printfn "Button 1 pressed! (args: %s)" (argsStr msg.arguments)
-        }))
+        Path ("oscillator", [
+            oscillator "1"
+            oscillator "2"
+            oscillator "3"
+        ])
+        Path ("window", [
+            Method ("openurl", (fun msg -> async {
+                match msg.arguments with
+                | [OscString urlStr] ->
+                    let uri = Uri urlStr
+                    if uri.Scheme = "http" || uri.Scheme = "https" then
+                        Process.Start (ProcessStartInfo(uri.ToString(), UseShellExecute = true, Verb = "Open")) |> ignore
+                    else
+                        eprintfn "Url scheme must be http or https"
+                | _ -> ()
+            }))
+        ])
     ])
 
 server.Run ()
